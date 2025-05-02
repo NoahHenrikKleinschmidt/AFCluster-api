e1b29d40777da69b73a30244fb823872e2f9ee24
Noah Kleinschmidt <Noah.kleinschmidt@unibe.ch>
2025-05-02 16:06:28 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="afcluster",
-    version="0.1.1",
+    version="0.1.2",
     description="Cluster Multiple-Sequence Alignments (MSA) using DBSCAN",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
